1332b8fe3a7356ee4c59d3c1acce7579320f0815
ferryal fajar <ferryalmfajar@gmail.com>
2021-11-06 19:55:44 +0700

    feat: add scrollable component

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,5 +1,8 @@
 import Delayed from './delayed';
 import Github from './github';
 import Loading from './loading';
+import Scroll from './scroll';
 
-export { Delayed, Github, Loading };
+export {
+  Delayed, Github, Loading, Scroll,
+};
